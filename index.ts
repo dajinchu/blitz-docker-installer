21a50af0896f55d2b870642cbea7e1191d692adf
dajinchu <dajinchu@gmail.com>
2020-05-20 02:57:49 -0400

    init

---
+++ index.ts
@@ -0,0 +1,22 @@
+import * as path from 'path'
+import { Installer } from '@blitzjs/installer'
+
+enum Steps {
+  addDockerfile
+}
+
+export default new Installer({
+  packageName: "blitz-docker",
+  packageDescription: "This installer adds a dockerfile to conatinerize your Blitz application",
+  packageRepoLink: "TODO",
+  packageOwner: "dajinchu@gmail.com"
+}, [
+  {
+    stepId: Steps.addDockerfile,
+    stepName: "Add dockerfile",
+    explanation: "Add the dockerfile",
+    templatePath: path.join(__dirname, "templates", "Dockerfile"),
+    targetDirectory: ".",
+    templateValues: {}
+  }
+])
